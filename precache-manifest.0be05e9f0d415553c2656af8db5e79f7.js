385498afcbfac5ad23efebe530275523aa1b2145
amrihyogi95 <demonshunter90@gmail.com>
2020-10-27 16:13:43 +0700

    Updates

---
+++ precache-manifest.0be05e9f0d415553c2656af8db5e79f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd58a153be867e9a0b8df8bcd439b831",
+    "url": "/moviebank/index.html"
+  },
+  {
+    "revision": "73b509b4e52856877e9e",
+    "url": "/moviebank/static/css/2.da1805c8.chunk.css"
+  },
+  {
+    "revision": "cc73f973a1761073d242",
+    "url": "/moviebank/static/css/main.6e276803.chunk.css"
+  },
+  {
+    "revision": "73b509b4e52856877e9e",
+    "url": "/moviebank/static/js/2.6fc15dc1.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/moviebank/static/js/2.6fc15dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc73f973a1761073d242",
+    "url": "/moviebank/static/js/main.341e9254.chunk.js"
+  },
+  {
+    "revision": "c0ea77e4a8847595d221",
+    "url": "/moviebank/static/js/runtime-main.605e0c0a.js"
+  },
+  {
+    "revision": "4b8a5cada780fd374b393f9281e07cc1",
+    "url": "/moviebank/static/media/back-button.4b8a5cad.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/moviebank/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/moviebank/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/moviebank/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/moviebank/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/moviebank/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
